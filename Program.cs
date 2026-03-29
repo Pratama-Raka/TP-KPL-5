d0f3763e8b3ac7dc89635fa7a477596f82908ee4
Raka Putra <pratamapoetra14@gmail.com>
2026-03-29 20:38:18 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class DataGeneric<T>
+{
+    public T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Isi data input
+        string nim = "103022400077";
+        // Membuat objek DataGeneric
+        DataGeneric<string> data = new DataGeneric<string>(nim);
+        // Memanggil method PrintData
+        data.PrintData();
+    }
+}
